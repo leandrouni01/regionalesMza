2d50003c3a4492148f274d16d543e5cf909d6e94
Gabriel Pérez <juan.gabriel.ps@gmail.com>
2019-08-22 20:25:44 -0300

    Lograda la conexion URL

--- app/src/main/java/com/iesmb/horticulturamza/MainActivity.java
+++ app/src/main/java/com/iesmb/horticulturamza/MainActivity.java
@@ -3,6 +3,7 @@ package com.iesmb.horticulturamza;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -30,27 +33,27 @@ public class MainActivity extends AppCompatActivity {
                         getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo informacioN = connection.getActiveNetworkInfo();
                 if(informacioN!= null && informacioN.isConnected()){
-                    try{
-                        URL url= new URL("https://swapi.co/api/people/1/");
-                        try{
-                            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                            try{
-                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                                int obj = in.read();
-                                TextView textView = findViewById(R.id.tex);
-                                textView.setText(obj);
-                            }catch (IOException e){
-                                e.printStackTrace();
-                            }finally {
-                                urlConnection.disconnect();
-                            }
-                        }catch (IOException em){
-                            em.printStackTrace();
+                    try {
+                        //URL url = new URL("https://www.google.com.ar");
+                        URL url = new URL("https://swapi.co/api/people/1/");
+                        try {
+                            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                            httpConn.setRequestMethod("GET");
+                            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                            StrictMode.setThreadPolicy(policy);
+                            InputStream inputStream = httpConn.getInputStream();
+                            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                            String line = bufferedReader.readLine();
+                            TextView textView = findViewById(R.id.tex);
+                            textView.setText(line);
+                            httpConn.disconnect();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    }catch (MalformedURLException ex){
-                        ex.printStackTrace();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
                     }
-
                 }
             }
         });
